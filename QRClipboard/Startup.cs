60f9580e7a883d68d9c3792d171b48d432e1affd
Mark Huynh <mark@markhuynh.com>
2021-01-06 23:03:54 -0500

    fix build error

--- QRClipboard/Startup.cs
+++ QRClipboard/Startup.cs
@@ -37,12 +37,10 @@ namespace QRClipboard
             }
             else
             {
-                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
